93666d71af4dd46ad8883dc6395d4ed3aebc9094
Douglas Vieira <nck77630@gmail.com>
2024-10-24 17:02:39 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles';
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/n1ckDoug.png" alt="" />
+);
+
+export default Avatar;
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar';
+import Paragrafo from '../../components/Paragrafo';
 import Titulo from '../../components/Titulo';
+
+import { Descricao, BotaoDescricao, SidebarContainer } from './styles';
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/n1ckDoug.png" alt="" />
-    <Titulo fontSize={20}>Douglas Vieira</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={18}>Douglas Vieira</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        n1ckDoug
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev. Full-Stack
+      </Descricao>
+      <BotaoDescricao>Trocar tema</BotaoDescricao>
+    </SidebarContainer>
   </aside>
 );
 
